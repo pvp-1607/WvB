80d7f3cc26e194c69f0bcb25e8c93e11758a92bc
pvp-1607 <77491889+pvp-1607@users.noreply.github.com>
2021-08-21 21:52:38 +0530

    Add files via upload

---
+++ New Browser/vaccine.js
@@ -0,0 +1,21 @@
+<html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+    <title>MapmyIndia Maps:Covid Places!</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <link rel="icon" href="https://www.mapmyindia.com/images/favicon.ico" type="image/x-icon">
+     <script src="https://maps.mapmyindia.com/covid-places/showNearbyCovidPlacesOnMap.js?category=HSPVCC"></script>
+   <style>
+   #nearby-vaccine-centres-mapmyindia{margin: 0;padding: 0;argin-left:0%;margin-top:0%;width: 100%;height: 99%;}
+   </style>
+   <script language="javascript">
+   </script>
+</head>
+<body >
+   <!--1. put your map container here-->
+   <div id="nearby-vaccine-centres-mapmyindia"></div>
+        <script type="text/javascript">
+        loadNearbyCovidCentresMapmyIndiaDiv(document.getElementById("nearby-vaccine-centres-mapmyindia"),{category1:'HSPSCC'});
+    </script>
+</body>                                                                                                                          
+</html>
